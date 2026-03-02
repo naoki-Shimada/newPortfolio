40f3f8721afecefb75e7fd0bcba3c244b63b46fb
naoki-Shimada <45224370+naoki-Shimada@users.noreply.github.com>
2026-03-02 20:01:27 +0900

    Delete scripts directory

--- scripts/getUserInfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-header('Content-Type: application/json');
-echo json_encode([
-    'isLoggedIn' => isset($_SESSION['userName']),
-    'userName' => $_SESSION['userName'] ?? 'ゲスト'
-]);
-?>
